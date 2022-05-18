ff5368733da38795519e5ee2ef1cc741856639b3
axelwass <axelwass@gmail.com>
2022-05-18 14:59:46 +0200

    to unweighted

--- utils/rmat.py
+++ utils/rmat.py
@@ -13,14 +13,15 @@ def rmat_to_file(N, E, a, b, c, d, dataset_folder, s):
   reduce = np.power(2, scale) - N
 
   Graph = nk.generators.RmatGenerator(scale, factor, a, b, c, d, weighted = True, reduceNodes = reduce).generate()
-  #Graph = nk.graph.Graph(Graph, False, False) # To undirected and unweigted
+  Graph = nk.graph.Graph(Graph, False, False) # To undirected and unweigted
   Graph.removeSelfLoops()
   #Graph = nk.components.ConnectedComponents(Graph).extractLargestConnectedComponent(Graph, compactGraph = True)
-  out_filename = Path(dataset_folder,'graphs','RMAT_{}.txt'.format(s))
+  name = 'RMAT_{}.txt'.format(s)
+  out_filename = Path(dataset_folder,'graphs',name)
   print("Wrinting to:" + str(out_filename))
   nk.writeGraph(Graph, str(out_filename), nk.Format.EdgeListTabOne)
   with lock:
     add_to_csv(Path(dataset_folder,"dataset_description.csv"), {
-      'N': N, 'E':E, 'a': a, 'b': b, 'c': c, 'd': d, 'name': out_filename, 'scale': scale, 'factor': factor, 'reduce': reduce
+      'N': N, 'E':E, 'a': a, 'b': b, 'c': c, 'd': d, 'name': name, 'scale': scale, 'factor': factor, 'reduce': reduce
     })
   return s
